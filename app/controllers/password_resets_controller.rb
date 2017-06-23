90b895026d20c72e9a40a735b11abd2e01a79cd5
eric <hyongzhe@mails.ccsf.edu>
2017-06-22 22:59:10 -0700

    add check expiration

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -50,4 +50,11 @@ class PasswordResetsController < ApplicationController
     end
   end
 
+  def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "Password reset has expired."
+        redirect_to new_password_reset_url
+      end
+    end
+
 end
