851973e15b17c3862078a424cc7e9ab573a3c04e
eric <hyongzhe@mails.ccsf.edu>
2017-06-22 21:33:06 -0700

    add password reset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
  config.action_mailer.delivery_method = :smtp
  host = 'frozen-earth-84938.herokuapp.com'
- config.action_mailer.default_url_options = { host: host }
+ config.action_mailer.default_wweweewwwwwwwssxedxurl_options = { host: host }
  ActionMailer::Base.smtp_settings = {
    :address        => 'smtp.sendgrid.net',
    :port           => '587',
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_reset/new'
+
+  get 'password_reset/edit'
+
   get 'sessions/new'
 
   root 'static_pages#home'
@@ -9,4 +13,6 @@ Rails.application.routes.draw do
   delete '/logout',to:'sessions#destroy'
   resources :users
   resources :account_activations,only: [:edit]
+  resources :password_resets,only: [:new,:create,:edit,:update]
+  
 end
