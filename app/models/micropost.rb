75566bb5deefc1d7cf5e521217e6880853bea204
eric <hyongzhe@mails.ccsf.edu>
2017-06-23 21:35:15 -0700

    add relationship

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,12 +4,5 @@ class Micropost < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :user_id,presence:true
   validates :content,presence:true,length:{maximum:140}
-  validate :picutre_size
-
-  private
-  def picture_size
-    if picture_size > 1.megabytes
-      errors.add(:picture,"should be smaller than 1MB")
-    end
-  end
+  
 end
